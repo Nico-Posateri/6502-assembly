da88c2ee478ed842106c8abe8fc554b10997d2c5
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-01-23 11:32:50 -0600

    Clears cart memory from $00 to $FF

--- 1-cleanmem.asm
+++ 1-cleanmem.asm
@@ -40,4 +40,3 @@ MemLoop:
 ;; To assemble, enter: dasm "FILE NAME".asm -f3 -v0 -ocart.bin
 ;; In Windows, you MUST include .asm extension, bin is named cart
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
