150111a36f51f62bab842b619f352274095cc467
Nico Posateri <141705409+Nico-Posateri@users.noreply.github.com>
2024-01-23 11:35:23 -0600

    Displays full NTSC spectrum background colors

--- 3-rainbow.asm
+++ 3-rainbow.asm
@@ -87,4 +87,3 @@ LoopOverscan:
 ;; To assemble, enter: dasm "FILE NAME".asm -f3 -v0 -ocart.bin
 ;; In Windows, you MUST include .asm extension, bin is named cart
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
